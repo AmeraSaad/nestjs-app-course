ee5cfab677a4c23bbe2f400c0fe05e8ce343142b
Amera Abdelaziz <amera.abdelaziz@areebgroup.com>
2026-04-12 15:52:13 +0200

    refactor: clean up product controller methods for consistency

--- src/products/productt.controller.ts
+++ src/products/productt.controller.ts
@@ -10,39 +10,41 @@ import { Controller,
 
 import { createProductDto } from "./dtos/create-product.dto";
 import { UpdateProductDto } from "./dtos/update-product.dto";
+import { ProductService } from "./product.service";
 
 
 @Controller('api/products')
 export class ProductController{
+    //@Imp note: Will use Dependency Injection later
+    private productService: ProductService = new ProductService();
 
     // POST: ~/api/products
     @Post()
     public CreateNewProduct(@Body() body:createProductDto){
-
+        return this.productService.CreateProduct(body);
     }
 
     // GET: ~/api/products
     @Get()
     public getAllProducts(){
-
+        return this.productService.getAll();
     }
 
     // GET: ~/api/products/:id
     @Get(':id')
     public getSingleProducts(@Param("id", ParseIntPipe) id: number){
-        
+        return this.productService.getOneBy(id);
     }
 
     // PUT: ~/api/products/:id
     @Put(':id')
     public updateProduct( @Param("id",ParseIntPipe) id: number,  @Body() body:UpdateProductDto){
-        
+        return this.productService.update(id, body);
     }
 
     // DELETE: ~/api/products/:id
     @Delete(':id')
     public deleteProduct(@Param("id",ParseIntPipe) id: number){
-
-    
+        return this.productService.delete(id);
     }
 }
