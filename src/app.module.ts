b12276ab88ea8898472bafbd0f8cbfe7b5b1090f
Amera Abdelaziz <amera.abdelaziz@areebgroup.com>
2026-04-19 16:21:12 +0200

    feat: configure environment variables and refactor TypeORM connection to use ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,31 @@ import { ProductsModule } from './products/products.module';
 import { ReviewsModule } from './reviews/reviews.module';
 import { UserModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Product } from './products/product.entity';
 
 @Module({
   imports: [
     ProductsModule,
     ReviewsModule, 
     UserModule,
-    TypeOrmModule.forRoot({
-        host: 'localhost',
-        type: 'postgres',
-        database: 'nestjs-app-db',
-        port: 5432,
-        username: 'postgres',
-        password:'123456',
-        synchronize: true, //only in development
-        autoLoadEntities: true,
-        entities:[],
-      })
+    TypeOrmModule.forRootAsync({
+        inject: [ConfigService],
+        useFactory: async (config: ConfigService) => ({
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          port: config.get<number>('DB_PORT'),
+          username: config.get<string>('DB_USERNAME'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_DATABASE'),
+          entities: [Product],
+          synchronize: process.env.NODE_ENV !== 'production',
+        }),
+      }),
+      ConfigModule.forRoot({ 
+        isGlobal: true,
+        envFilePath: `.env.${process.env.NODE_ENV}`,
+      }),
     ],
 
 
