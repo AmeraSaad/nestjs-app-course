614f228d7dcf3bdb498ecb87bc3a6a5b050f64a5
Amera Abdelaziz <amera.abdelaziz@areebgroup.com>
2026-04-20 10:17:55 +0200

    feat: add JwtModule for JWT authentication in UserModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,25 @@ import { UsersController } from "./users.controller";
 import { UserService } from "./users.service";
 import { User } from "./user.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtModule } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { StringValue } from "ms";
 
 @Module({
     controllers: [UsersController],
     providers:[UserService],
-    imports: [TypeOrmModule.forFeature([User])]
+    imports: [
+        TypeOrmModule.forFeature([User]),
+        JwtModule.registerAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+              global: true,
+              secret: configService.getOrThrow<string>("JWT_SECRET"),
+              signOptions: {
+                expiresIn: configService.getOrThrow<string>("JWT_EXPIRES_IN") as StringValue
+              },
+            }),
+        })
+    ]
 })
 export class UserModule {}
